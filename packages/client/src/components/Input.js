25da25261298480321d14a8101f3c7574be5b6be
Mauricio Mutte <litiobr@gmail.com>
2019-07-29 23:48:14 -0300

    Add Input

---
+++ packages/client/src/components/Input.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Input } from './styled';
+
+export default () => 
+  <Input placeholder="Type anything"/>
--- packages/client/src/components/styled.js
+++ packages/client/src/components/styled.js
@@ -52,3 +52,12 @@ export const Text = styled.span`
   display: block;
   font-weight: lighter;
 `;
+
+export const Input = styled.input`
+  border: none;
+  border-radius: 30px;
+  box-shadow: 1px 2px 1px #0003;
+  padding: 0 15px;
+  width: 100%;
+  max-width: 600px;
+`;
