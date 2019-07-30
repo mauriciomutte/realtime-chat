798f7abd35b709ba2e5ebadd41edc4955f5f6a7a
Mauricio Mutte <litiobr@gmail.com>
2019-07-30 10:54:18 -0300

    Move Message to Main

--- packages/client/src/main/index.js
+++ packages/client/src/main/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import UsersOnline from '../components/UsersOnline'
-import Chat from '../components/Chat'
 import Send from '../components/Send'
+import Message from '../components/Message';
 import GlobalStyled from '../globalStyled';
+import { Chat } from '../components/styled';
 import * as S from './styled'
 
 export default class Main extends Component {
@@ -11,7 +12,12 @@ export default class Main extends Component {
       <S.Main>
         <GlobalStyled />
         <UsersOnline />
-        <Chat />
+        <Chat>
+          <Message user="Maurício" me={true} txt="Que bom, fico feliz em ver vocês bem!"/>
+          <Message user="Maithê" me={false} txt="Olá, Maurício! Tudo bem e com você?"/>
+          <Message user="Pitoco" me={false} txt="Au au au!"/>
+          <Message user="Maurício" me={true} txt="Olá pessoal! Como vocês estão?"/>
+        </Chat>
         <Send />
       </S.Main>
     );
