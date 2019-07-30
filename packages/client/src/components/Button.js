d237db84271083a89ac7ce2ba5b3f75c3d32cca3
Mauricio Mutte <litiobr@gmail.com>
2019-07-29 23:48:24 -0300

    Add Button

---
+++ packages/client/src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from './styled';
+
+export default () => 
+  <Button>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="#9E9E9E" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/>
+      <path d="M0 0h24v24H0z" fill="none"/>
+    </svg>
+  </Button>
--- packages/client/src/components/styled.js
+++ packages/client/src/components/styled.js
@@ -61,3 +61,15 @@ export const Input = styled.input`
   width: 100%;
   max-width: 600px;
 `;
+
+export const Button = styled.button`
+  align-items: center;
+  border: none;
+  border-radius: 100%;
+  box-shadow: 1px 2px 1px #0003;
+  background-color: #FFF;
+  cursor: pointer;
+  display: flex;
+  margin: 0 0 0 10px;
+  padding: 0 8px 0 10px;
+`;
