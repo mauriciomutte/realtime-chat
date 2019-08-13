48393adce37f35e74b4e25dc7f7da1d00cb78444
Mauricio Mutte <litiobr@gmail.com>
2019-08-13 20:25:10 -0300

    feat(hooks): add hooks

--- packages/client/src/main/index.js
+++ packages/client/src/main/index.js
@@ -1,115 +1,96 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import UsersOnline from '../components/UsersOnline';
 import SendInput from '../components/SendInput';
 import SendButton from '../components/SendButton';
 import Message from '../components/Message';
 import GlobalStyled from '../globalStyled';
 import * as S from './styled';
-import { 
-  Chat, 
-  Send, 
-  Title, 
-  EnterUsername, 
-  Box, 
-  Input, 
-  Button 
+import {
+  Chat,
+  Send,
+  Title,
+  EnterUsername,
+  Box,
+  Input,
+  Button
 } from '../components/styled';
 
 import socketIOClient from 'socket.io-client';
 const socket = socketIOClient('http://localhost:4000');
 
-const initialState = {
-  usersOnline: 0,
-  users: [],
-  username: '', 
-  hasUsername: false,
-  value: '',
-  messages: []
-};
+export default () => {
+  const [usersOnline, setUsersOnline] = useState(0);
+  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState('');
+  const [hasUsername, setHasUsername] = useState(false);
+  const [value, setValue] = useState('');
+  const [messages, setMessages] = useState([]);
 
-export default class Main extends Component {
-  state = { ...initialState };
+  socket.on('messages', messages => setMessages(messages));
+  socket.on('usersOnline', usersOnline => setUsersOnline(usersOnline));
+  socket.on('users', users => setUsers(users));
 
-  constructor(props) {
-    super(props);
-
-    socket.on('messages', messages => this.setState({ messages }));
-    socket.on('usersOnline', usersOnline => this.setState({ usersOnline }));
-    socket.on('users', users => this.setState({ users }));
-
-    this.sendMsg = this.sendMsg.bind(this);
-    this.handleChangeSend = this.handleChangeSend.bind(this);
-    this.handleChangeUser = this.handleChangeUser.bind(this);
-    this.keyPressedSend = this.keyPressedSend.bind(this);
-    this.keyPressedUser = this.keyPressedUser.bind(this);
-    this.getUsername = this.getUsername.bind(this);
+  function handleChangeSend(event) {
+    setValue(event.target.value);
   }
 
-  handleChangeSend(event) {
-    this.setState({value: event.target.value});
+  function handleChangeUser(event) {
+    setUsername(event.target.value);
   }
 
-  handleChangeUser(event) {
-    this.setState({username: event.target.value});
+  function keyPressedSend(event) {
+    if (event.key === "Enter") { sendMsg(); }
   }
 
-  keyPressedSend(event) { 
-    if (event.key === "Enter") { this.sendMsg(); }
+  function keyPressedUser(event) {
+    if (event.key === "Enter") { getUsername(); }
   }
 
-  keyPressedUser(event) { 
-    if (event.key === "Enter") { this.getUsername(); }
-  }
-
-  getUsername() {
-    this.state.users.unshift({
-      username: this.state.username
+  function getUsername() {
+    users.unshift({
+      username: username
     });
 
-    socket.emit('users', this.state.users);
-    this.setState({hasUsername: true}); 
+    socket.emit('users', users);
+    setHasUsername(true);
   }
 
-  sendMsg() {
-    this.state.messages.unshift({
-      username: this.state.username,
-      txt: this.state.value,
+  function sendMsg() {
+    messages.unshift({
+      username: username,
+      txt: value,
       date: new Date(),
     });
 
-    socket.emit('messages', this.state.messages);
+    socket.emit('messages', messages);
 
-    this.setState({value: ''});
+    setValue('');
   }
-  
-  render() {
-    const {usersOnline, username, hasUsername, value, messages} = this.state;
 
-    return (
-      <S.Main>
-        <GlobalStyled />
-        <EnterUsername username={hasUsername}>
-          <Box>
-            <Title color={'#4286F5'}>Enter with your username</Title>
-            <Input onChange={this.handleChangeUser} onKeyPress={this.keyPressedUser} autoFocus/>
-            <Button onClick={this.getUsername}>Join</Button>
-          </Box>
-        </EnterUsername>
-        <UsersOnline users={usersOnline}/>
-        <Chat>
-          {messages.map(message => (
-            <Message 
-              user={message.username} 
-              txt={message.txt} 
-              me={message.username === username ? true : false}
-            />
-          ))}
-        </Chat>
-        <Send>
-          <SendInput value={value} handleChange={this.handleChangeSend} onKeyPress={this.keyPressedSend}/>
-          <SendButton click={this.sendMsg} />
-        </Send>
-      </S.Main>
-    );
-  }
+  return (
+    <S.Main>
+      <GlobalStyled />
+      <EnterUsername username={hasUsername}>
+        <Box>
+          <Title color={'#4286F5'}>Enter with your username</Title>
+          <Input onChange={handleChangeUser} onKeyPress={keyPressedUser} autoFocus />
+          <Button onClick={getUsername}>Join</Button>
+        </Box>
+      </EnterUsername>
+      <UsersOnline users={usersOnline} />
+      <Chat>
+        {messages.map(message => (
+          <Message
+            user={message.username}
+            txt={message.txt}
+            me={message.username === username ? true : false}
+          />
+        ))}
+      </Chat>
+      <Send>
+        <SendInput value={value} handleChange={handleChangeSend} onKeyPress={keyPressedSend} />
+        <SendButton click={sendMsg} />
+      </Send>
+    </S.Main>
+  );
 }
